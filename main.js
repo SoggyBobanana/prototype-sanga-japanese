ee4886f3433a56f195cf52056f2d6e896b34c1e1
mochazzt <166436942+SoggyBobanana@users.noreply.github.com>
2026-05-14 12:09:18 +0000

    added js for fake database and css for admin page

---
+++ main.js
@@ -0,0 +1,31 @@
+// customer list (fake database)
+let customers = []
+
+// Add customer
+function addCustomer(name){
+    customers.push(name);
+    console.log("Added", name);
+    displayCustomers(); 
+} 
+
+// Show customers
+function displayCustomers(){
+    console.log("Customer List:");
+    customers.forEach((customer, index) => {
+        console.log(index + 1 + ". " + c);
+    });
+}
+
+// Delete customer
+function deleteCustomer(index){
+    let removed = customers.splice(index, 1);
+    console.log("Deleted", removed[0]);
+    displayCustomers();
+}
+
+// Testing prototype
+addCustomer("Alice");
+addCustomer("Bob");
+addCustomer("Charlie");
+
+deleteCustomer(1); 
